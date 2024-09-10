2a85c92ba898ee51c85188df3c712ea84bd1d0b0
Daniel Marhcenko <marchenkodanil97@gmail.com>
2024-09-10 15:08:52 +0300

    Add middlewares with regexp

--- src/server.js
+++ src/server.js
@@ -12,10 +12,40 @@ app.use((req, res, next) => {
 
 app.use(express.json());
 
+// ----- RegExp Middlewares
+
+// Middleware will be applied to paths starting with /products and having an optional /category
+app.use(/^\/products(\/category)?$/, (req, res, next) => {
+  console.log('Middleware for products or category');
+  next();
+});
+
+// Middleware will be applied to paths which have UUID at the end
+app.use(
+  /^\/user\/([a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12})$/,
+  (req, res, next) => {
+    console.log('Middleware for route with UUID');
+    next();
+  },
+);
+
+// ----- Routes
 app.get('/', (req, res) => {
   res.json({
     message: 'Hello, World!',
   });
+});
+
+app.get('/products', (req, res) => {
+  res.send('Show all products');
+});
+
+app.get('/products/category', (req, res) => {
+  res.send('Show all products by category');
+});
+
+app.get('/user/123e4567-e89b-12d3-a456-426614174000', (req, res) => {
+  res.send('User profile');
 });
 
 app.listen(PORT, () => {
