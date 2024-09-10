39641e41d7902f44375a81f8cac2074110b260dc
Daniel Marhcenko <marchenkodanil97@gmail.com>
2024-09-10 15:09:23 +0300

    Add not found and error handler middlewares

--- src/server.js
+++ src/server.js
@@ -48,6 +48,20 @@ app.get('/user/123e4567-e89b-12d3-a456-426614174000', (req, res) => {
   res.send('User profile');
 });
 
+// ----- Not Found and Error Handler middlewares
+app.use('*', (req, res, next) => {
+  res.status(404).json({
+    message: 'Not found',
+  });
+});
+
+app.use((err, req, res, next) => {
+  res.status(500).json({
+    message: 'Something went wrong',
+    error: err.message,
+  });
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
