b0ddab45834902be94d52bbeaad7e4a27ce69ee0
Daniel Marhcenko <marchenkodanil97@gmail.com>
2024-09-10 15:41:29 +0300

    Create function to start server

--- src/server.js
+++ src/server.js
@@ -6,38 +6,40 @@ import { ENV_VARS } from './constants/index.js';
 
 const PORT = Number(env(ENV_VARS.PORT, '3000'));
 
-const app = express();
+export const startServer = () => {
+  const app = express();
 
-app.use(express.json());
-app.use(cors());
+  app.use(express.json());
+  app.use(cors());
 
-app.use(
-  pino({
-    transport: {
-      target: 'pino-pretty',
-    },
-  }),
-);
+  app.use(
+    pino({
+      transport: {
+        target: 'pino-pretty',
+      },
+    }),
+  );
 
-app.get('/', (req, res) => {
-  res.json({
-    message: 'Hello, World!',
+  app.get('/', (req, res) => {
+    res.json({
+      message: 'Hello, World!',
+    });
   });
-});
 
-app.use('*', (req, res, next) => {
-  res.status(404).json({
-    message: 'Not found',
+  app.use('*', (req, res, next) => {
+    res.status(404).json({
+      message: 'Not found',
+    });
   });
-});
 
-app.use((err, req, res, next) => {
-  res.status(500).json({
-    message: 'Something went wrong',
-    error: err.message,
+  app.use((err, req, res, next) => {
+    res.status(500).json({
+      message: 'Something went wrong',
+      error: err.message,
+    });
   });
-});
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+};
