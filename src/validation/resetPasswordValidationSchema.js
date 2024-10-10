995ed1ed08be0c23d58c12dd5e9c317f6f8035f6
Borys Meshkov <100526181+BorysMeshkov@users.noreply.github.com>
2024-10-10 19:52:22 +0300

    test

--- src/validation/resetPasswordValidationSchema.js
+++ src/validation/resetPasswordValidationSchema.js
@@ -2,5 +2,5 @@ import Joi from 'joi';
 
 export const resetPasswordValidationSchema = Joi.object({
   token: Joi.string().required(),
-  password: Joi.string().required().min(6).max(24),
+  password: Joi.string().required().min(6).max(20),
 });
